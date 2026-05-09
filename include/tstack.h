878431aef0757bc98374b859b783a7f2df9e72f0
leraerra <valeriaberbasova@yandex.ru>
2026-05-09 11:42:31 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,45 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
+#include <stdexcept>
+
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  T arr[size];
+  int top_index;
+
+public:
+  TStack() : top_index(-1) {}
+
+  bool isEmpty() const {
+    return top_index == -1;
+  }
+
+  bool isFull() const {
+    return top_index == size - 1;
+  }
+
+  void push(const T &item) {
+    if (isFull()) {
+      throw std::out_of_range("Stack overflow");
+    }
+    arr[++top_index] = item;
+  }
+
+  T pop() {
+    if (isEmpty()) {
+      throw std::out_of_range("Stack underflow");
+    }
+    return arr[top_index--];
+  }
+
+  T get() const {
+    if (isEmpty()) {
+      throw std::out_of_range("Stack underflow");
+    }
+    return arr[top_index];
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
